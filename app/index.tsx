fbd9d2e85faa7426e6a0ebf856474b43c2d0aee0
coder-j25 <justinee.hanson@outlook.com>
2025-05-14 06:26:19 -0700

    initial commit with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text, View } from "react-native";
+
+export default function Index() {
+  return (
+    <View
+   className="items-center justify-center flex-1">
+      <Text className="text-5xl font-bold text-dark-200">Welcome!</Text>
+    </View>
+  );
+}
